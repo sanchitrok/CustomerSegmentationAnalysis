71fc01424426f33f0678227439a88b59d218925e
Sanchit Rokade <rokadesr4@gmail.com>
2020-03-27 00:36:23 -0500

    Add files via upload

---
+++ Clustering.R
@@ -0,0 +1,70 @@
+#Clustering
+# K-Means Clustering
+# Importing the dataset
+dataset = read.csv('Mall_Customers.csv')
+dataset = dataset[4:5]
+
+# Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
+
+# Fitting K-Means to the dataset
+set.seed(29)
+kmeans = kmeans(x = dataset, centers = 6)
+y_kmeans = kmeans$cluster
+
+# Visualising the clusters
+library(cluster)
+clusplot(dataset,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = FALSE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
+
+# Visualising the cluster Center
+plot(dataset, col = kmeans$cluster, main = "K-Means with 5 clusters")
+kmeans$centers
+points(kmeans$centers, col = 1:3,pch = 8, cex=3)#cex = font size, pch = symbol
+table(y_kmeans)
+
+# Hierarchical Clustering
+# Using the dendrogram to find the optimal number of clusters
+dendrogram = hclust(d = dist(dataset, method = 'euclidean'), method = 'ward.D')
+plot(dendrogram,
+     main = paste('Dendrogram'),
+     xlab = 'Customers',
+     ylab = 'Euclidean distances')
+
+# Fitting Hierarchical Clustering to the dataset
+hc = hclust(d = dist(dataset, method = 'euclidean'), method = 'ward.D')
+y_hc = cutree(hc, 6)
+rect.hclust(hc, k=6, border="red")
+
+# Visualising the clusters
+library(cluster)
+clusplot(dataset,
+         y_hc,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels= 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
+
+table(y_hc)
